71d37aa3f72af6e4f4f7f84ce79ae89594a02660
Akash Sondarva <akash.sondarva@bacancy.com>
2023-03-20 15:59:30 +0530

    test commit

--- newtodo/src/App.js
+++ newtodo/src/App.js
@@ -5,85 +5,81 @@ import List from './components/List';
 import { useState } from 'react';
 import Edit from './components/Edit';
 
-
 function App() {
-
-  const [data, setData] = useState([])
-  const [isEditing,setIsEditing] = useState(false)
-  const [current,setCurrent] = useState({})
+  // test
+  const [data, setData] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [current, setCurrent] = useState({});
 
   const newItem = (newValue) => {
-       
-      setData((oldvalue) =>{
-        return [{
-          id:Math.random().toString(),
-          title:newValue.title,
-          des:newValue.des
-        },...oldvalue]
-      })
-  }
+    setData((oldvalue) => {
+      return [
+        {
+          id: Math.random().toString(),
+          title: newValue.title,
+          des: newValue.des,
+        },
+        ...oldvalue,
+      ];
+    });
+  };
 
   const onEdit = (editValue) => {
-      // console.log(editValue)
-      setIsEditing(true)
-      setCurrent({...editValue})
-      // console.log(current)
-  }
+    // console.log(editValue)
+    setIsEditing(true);
+    setCurrent({ ...editValue });
+    // console.log(current)
+  };
 
   const onDelete = (DeletValue) => {
-     setData((oldvalue) => {
-       const removeData = oldvalue.filter((e) => e.id !== DeletValue)
-       return removeData
-     })
-  }
+    setData((oldvalue) => {
+      const removeData = oldvalue.filter((e) => e.id !== DeletValue);
+      return removeData;
+    });
+  };
 
-
-  const handleEditChnage  = (e) => {
-
+  const handleEditChnage = (e) => {
     setCurrent((oldvalue) => {
-      return {...oldvalue,[e.target.name]:e.target.value}
-    })
-
-   
-  }
+      return { ...oldvalue, [e.target.name]: e.target.value };
+    });
+  };
 
   const HandleEditSubmit = (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      if(current.title === "" ||  current.des === ""){
-        alert("Title and Task can not be empty")
-        
-      }
-      else{
-      setIsEditing(false)
+    if (current.title === '' || current.des === '') {
+      alert('Title and Task can not be empty');
+    } else {
+      setIsEditing(false);
 
       // console.log(current)
       const updateData = data.map((e) => {
-        return e.id === current.id ? current : e
-      })
+        return e.id === current.id ? current : e;
+      });
       // console.log(updateData)
-      setData(updateData)
+      setData(updateData);
     }
-  }
+  };
 
-
-  const cancleEdit = (e) =>{
+  const cancleEdit = (e) => {
     e.preventDefault();
-    setIsEditing(false)
-  }
-
+    setIsEditing(false);
+  };
 
   return (
     <>
-    {isEditing ? <Edit 
-    current={current}
-    HandleEditSubmit={HandleEditSubmit}
-    handleEditChnage ={handleEditChnage }
-    cancleEdit={cancleEdit}
-    /> : <Form newItem={newItem}/> }
-    
+      {isEditing ? (
+        <Edit
+          current={current}
+          HandleEditSubmit={HandleEditSubmit}
+          handleEditChnage={handleEditChnage}
+          cancleEdit={cancleEdit}
+        />
+      ) : (
+        <Form newItem={newItem} />
+      )}
 
-    <List data={data} delete={onDelete} edit={onEdit}/>    
+      <List data={data} delete={onDelete} edit={onEdit} />
     </>
   );
 }
